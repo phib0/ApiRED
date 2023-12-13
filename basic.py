fef9507f3df32418f6e158456358e562bd890e6f
JoshuaLnt <lehnertjoshua0@gmail.com>
2023-12-13 17:24:18 +0100

    Updated Interpreter, added Power Operator
    - added Power Operator '^' with negative-value support

--- basic.py
+++ basic.py
@@ -18,56 +18,79 @@ DIGITS = '0123456789'
 #######################################
 
 class Error:
-        def __init__(self, pos_start, pos_end, error_name, details):
-                self.pos_start = pos_start
-                self.pos_end = pos_end
-                self.error_name = error_name
-                self.details = details
-        
-        def as_string(self):
-                result  = f'{self.error_name}: {self.details}\n'
-                result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
-                result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
-                return result
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        self.error_name = error_name
+        self.details = details
+    
+    def as_string(self):
+        result  = f'{self.error_name}: {self.details}\n'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+        return result
 
 class IllegalCharError(Error):
-        def __init__(self, pos_start, pos_end, details):
-                super().__init__(pos_start, pos_end, 'Illegal Character', details)
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
 class InvalidSyntaxError(Error):
-        def __init__(self, pos_start, pos_end, details=''):
-                super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+    def __init__(self, pos_start, pos_end, details=''):
+        super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+
+class RTError(Error):
+    def __init__(self, pos_start, pos_end, details, context):
+        super().__init__(pos_start, pos_end, 'Runtime Error', details)
+        self.context = context
+
+    def as_string(self):
+        result  = self.generate_traceback()
+        result += f'{self.error_name}: {self.details}'
+        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos = self.pos_start
+        ctx = self.context
+
+        while ctx:
+            result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+
+        return 'Traceback (most recent call last):\n' + result
 
 #######################################
 # POSITION
 #######################################
 
 class Position:
-        def __init__(self, idx, ln, col, fn, ftxt):
-                self.idx = idx
-                self.ln = ln
-                self.col = col
-                self.fn = fn
-                self.ftxt = ftxt
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
 
-        def advance(self, current_char=None):
-                self.idx += 1
-                self.col += 1
+    def advance(self, current_char=None):
+        self.idx += 1
+        self.col += 1
 
-                if current_char == '\n':
-                        self.ln += 1
-                        self.col = 0
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
 
-                return self
+        return self
 
-        def copy(self):
-                return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
 
 #######################################
 # TOKENS
 #######################################
 
-TT_INT            = 'INT'
+TT_INT      = 'INT'
 TT_FLOAT    = 'FLOAT'
 TT_PLUS     = 'PLUS'
 TT_MINUS    = 'MINUS'
@@ -75,94 +98,98 @@ TT_MUL      = 'MUL'
 TT_DIV      = 'DIV'
 TT_LPAREN   = 'LPAREN'
 TT_RPAREN   = 'RPAREN'
-TT_EOF            = 'EOF'
+TT_POWER    = 'POWER'
+TT_EOF      = 'EOF'
 
 class Token:
-        def __init__(self, type_, value=None, pos_start=None, pos_end=None):
-                self.type = type_
-                self.value = value
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
+        self.type = type_
+        self.value = value
 
-                if pos_start:
-                    self.pos_start = pos_start.copy()
-                    self.pos_end = pos_start.copy()
-                    self.pos_end.advance()
+        if pos_start:
+            self.pos_start = pos_start.copy()
+            self.pos_end = pos_start.copy()
+            self.pos_end.advance()
 
-                if pos_end:
-                    self.pos_end = pos_end
-        
-        def __repr__(self):
-                if self.value: return f'{self.type}:{self.value}'
-                return f'{self.type}'
+        if pos_end:
+            self.pos_end = pos_end
+    
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
 
 #######################################
 # LEXER
 #######################################
 
 class Lexer:
-        def __init__(self, fn, text):
-                self.fn = fn
-                self.text = text
-                self.pos = Position(-1, 0, -1, fn, text)
-                self.current_char = None
+    def __init__(self, fn, text):
+        self.fn = fn
+        self.text = text
+        self.pos = Position(-1, 0, -1, fn, text)
+        self.current_char = None
+        self.advance()
+    
+    def advance(self):
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+            if self.current_char in ' \t':
                 self.advance()
-        
-        def advance(self):
-                self.pos.advance(self.current_char)
-                self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
-
-        def make_tokens(self):
-                tokens = []
-
-                while self.current_char != None:
-                        if self.current_char in ' \t':
-                                self.advance()
-                        elif self.current_char in DIGITS:
-                                tokens.append(self.make_number())
-                        elif self.current_char == '+':
-                                tokens.append(Token(TT_PLUS, pos_start=self.pos))
-                                self.advance()
-                        elif self.current_char == '-':
-                                tokens.append(Token(TT_MINUS, pos_start=self.pos))
-                                self.advance()
-                        elif self.current_char == '*':
-                                tokens.append(Token(TT_MUL, pos_start=self.pos))
-                                self.advance()
-                        elif self.current_char == '/':
-                                tokens.append(Token(TT_DIV, pos_start=self.pos))
-                                self.advance()
-                        elif self.current_char == '(':
-                                tokens.append(Token(TT_LPAREN, pos_start=self.pos))
-                                self.advance()
-                        elif self.current_char == ')':
-                                tokens.append(Token(TT_RPAREN, pos_start=self.pos))
-                                self.advance()
-                        else:
-                                pos_start = self.pos.copy()
-                                char = self.current_char
-                                self.advance()
-                                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
-
-                tokens.append(Token(TT_EOF, pos_start=self.pos))
-                return tokens, None
-
-        def make_number(self):
-                num_str = ''
-                dot_count = 0
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '^':
+                tokens.append(Token(TT_POWER, pos_start=self.pos))
+                self.advance()
+            else:
                 pos_start = self.pos.copy()
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
 
-                while self.current_char != None and self.current_char in DIGITS + '.':
-                        if self.current_char == '.':
-                                if dot_count == 1: break
-                                dot_count += 1
-                                num_str += '.'
-                        else:
-                                num_str += self.current_char
-                        self.advance()
+        tokens.append(Token(TT_EOF, pos_start=self.pos))
+        return tokens, None
 
-                if dot_count == 0:
-                        return Token(TT_INT, int(num_str), pos_start, self.pos)
-                else:
-                        return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
+    def make_number(self):
+        num_str = ''
+        dot_count = 0
+        pos_start = self.pos.copy()
+
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
+
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str), pos_start, self.pos)
+        else:
+            return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
 
 #######################################
 # NODES
@@ -171,7 +198,7 @@ class Lexer:
 class NumberNode:
     def __init__(self, tok):
         self.tok = tok
-        
+
         self.pos_start = self.tok.pos_start
         self.pos_end = self.tok.pos_end
 
@@ -183,9 +210,9 @@ class BinOpNode:
         self.left_node = left_node
         self.op_tok = op_tok
         self.right_node = right_node
-        
+
         self.pos_start = self.left_node.pos_start
-        self.pos_end = self.right_node.pos_start
+        self.pos_end = self.right_node.pos_end
 
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
@@ -194,7 +221,7 @@ class UnaryOpNode:
     def __init__(self, op_tok, node):
         self.op_tok = op_tok
         self.node = node
-        
+
         self.pos_start = self.op_tok.pos_start
         self.pos_end = node.pos_end
 
@@ -246,7 +273,7 @@ class Parser:
         if not res.error and self.current_tok.type != TT_EOF:
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Expected '+', '-', '*' or '/'"
+                "Expected '+', '-', '*', '/' or '^'"
             ))
         return res
 
@@ -285,7 +312,7 @@ class Parser:
         ))
 
     def term(self):
-        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV, TT_POWER))
 
     def expr(self):
         return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
@@ -305,7 +332,28 @@ class Parser:
             left = BinOpNode(left, op_tok, right)
 
         return res.success(left)
-    
+
+#######################################
+# RUNTIME RESULT
+#######################################
+
+class RTResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+
+    def register(self, res):
+        if res.error: self.error = res.error
+        return res.value
+
+    def success(self, value):
+        self.value = value
+        return self
+
+    def failure(self, error):
+        self.error = error
+        return self
+
 #######################################
 # VALUES
 #######################################
@@ -314,87 +362,145 @@ class Number:
     def __init__(self, value):
         self.value = value
         self.set_pos()
+        self.set_context()
 
     def set_pos(self, pos_start=None, pos_end=None):
         self.pos_start = pos_start
         self.pos_end = pos_end
         return self
+
+    def set_context(self, context=None):
+        self.context = context
+        return self
+
     def added_to(self, other):
         if isinstance(other, Number):
-            return Number(self.value + other.value)
-        
+            return Number(self.value + other.value).set_context(self.context), None
+
     def subbed_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value - other.value)
-    
+            return Number(self.value - other.value).set_context(self.context), None
+
     def multed_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value * other.value)
-        
+            return Number(self.value * other.value).set_context(self.context), None
+
     def dived_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value / other.value)
-    
+            if other.value == 0:
+                return None, RTError(
+                    other.pos_start, other.pos_end,
+                    'Division by zero',
+                    self.context
+                )
+
+            return Number(self.value / other.value).set_context(self.context), None
+        
+    def to_power_of(self, other):
+        if isinstance(other, Number):
+            prod = 1
+            if other.value < 0:
+                val = other.value *-1
+                for _ in range(val):
+                    prod *= self.value
+                return Number((1/prod)).set_context(self.context), None
+            elif other.value == 0:
+                return Number(1).set_context(self.context), None
+            else:
+                for _ in range(other.value):
+                    prod *= self.value
+                return Number(prod).set_context(self.context), None
+        
+
     def __repr__(self):
         return str(self.value)
-    
+
+#######################################
+# CONTEXT
+#######################################
+
+class Context:
+    def __init__(self, display_name, parent=None, parent_entry_pos=None):
+        self.display_name = display_name
+        self.parent = parent
+        self.parent_entry_pos = parent_entry_pos
+
 #######################################
 # INTERPRETER
 #######################################
 
 class Interpreter:
-    def visit(self, node):
+    def visit(self, node, context):
         method_name = f'visit_{type(node).__name__}'
         method = getattr(self, method_name, self.no_visit_method)
-        return method(node)
-    
-    def no_visit_method(self, node):
-        raise Exception(f'No visit_{type(node).__name__} method defined')
-    
-    def visit_NumberNode(self, node):
-        return Number(node.tok.value).set_pos(node.pos_start, node.pos_end)
-    
-        
-    def visit_BinOpNode(self, node):
-        left = self.visit(node.left_node)
-        right = self.visit(node.right_node)
-        
-        if node.op_tok.type == TT_PLUS:
-            result = left.added_to(right)
-        elif node.op_tok.type == TT_MINUS:
-            result = left.subbed_by(right)
-        elif node.op_tok.type == TT_MUL:
-            result = left.multed_by(right)
-        elif node.op_tok.type == TT_DIV:
-            result = left.dived_by(right)
-            
-        return result.set_pos(node.pos_start, node.pos_end)
+        return method(node, context)
 
-    def visit_UnaryOpNode(self, node):
-        number = self.visit(node.node)
-        
+    def no_visit_method(self, node, context):
+        raise Exception(f'No visit_{type(node).__name__} method defined')
+
+    ###################################
+
+    def visit_NumberNode(self, node, context):
+        return RTResult().success(
+            Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+        )
+
+    def visit_BinOpNode(self, node, context):
+        res = RTResult()
+        left = res.register(self.visit(node.left_node, context))
+        if res.error: return res
+        right = res.register(self.visit(node.right_node, context))
+        if res.error: return res
+
+        if node.op_tok.type == TT_PLUS:
+            result, error = left.added_to(right)
+        elif node.op_tok.type == TT_MINUS:
+            result, error = left.subbed_by(right)
+        elif node.op_tok.type == TT_MUL:
+            result, error = left.multed_by(right)
+        elif node.op_tok.type == TT_DIV:
+            result, error = left.dived_by(right)
+        elif node.op_tok.type == TT_POWER:
+            result, error = left.to_power_of(right)
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+    def visit_UnaryOpNode(self, node, context):
+        res = RTResult()
+        number = res.register(self.visit(node.node, context))
+        if res.error: return res
+
+        error = None
+
         if node.op_tok.type == TT_MINUS:
-            number = number.multed_by(Number(-1))
-            
-        return number.set_pos(node.pos_start, node.pos_end)
-        
+            number, error = number.multed_by(Number(-1))
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(number.set_pos(node.pos_start, node.pos_end))
+
 #######################################
 # RUN
 #######################################
 
 def run(fn, text):
-        # Generate tokens
-        lexer = Lexer(fn, text)
-        tokens, error = lexer.make_tokens()
-        if error: return None, error
-        
-        # Generate AST
-        parser = Parser(tokens)
-        ast = parser.parse()
-        if ast.error: return None, ast.error
+    # Generate tokens
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+    if error: return None, error
+    
+    # Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    if ast.error: return None, ast.error
 
-        # Run program
-        interpreter = Interpreter()
-        result = interpreter.visit(ast.node)
+    # Run program
+    interpreter = Interpreter()
+    context = Context('<program>')
+    result = interpreter.visit(ast.node, context)
 
-        return result, None
+    return result.value, result.error
