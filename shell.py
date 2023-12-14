091c6107e8fdbde52d100befa2da796e9ec05777
JoshuaLnt <lehnertjoshua0@gmail.com>
2023-12-14 13:55:17 +0100

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,9 +1,12 @@
 import basic
 
-if __name__ == '__main__':
+def run():
     while True:
         text = input('input -> ')
         result, error = basic.run('<stdin>', text)
 
         if error: print(error.as_string())
         else: print(result)
+
+if __name__ == '__main__':
+    run()
